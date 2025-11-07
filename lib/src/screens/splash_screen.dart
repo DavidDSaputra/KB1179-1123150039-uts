f1ead742c5637d2caaa4c3d79878e788541ad0b0
DavidDSaputra <davidsaputra2704@gmail.com>
2025-11-07 20:39:47 +0700

    timer login 2detik ke halaman login

--- lib/src/screens/splash_screen.dart
+++ lib/src/screens/splash_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import '../routes.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      if (!mounted) return;
+      Navigator.pushReplacementNamed(context, Routes.login);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
